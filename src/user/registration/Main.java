811f3713bf910fd7b253297f0345c5e6769a59c1
Pooja <pooja.14206@gmail.com>
2021-04-15 00:14:14 +0530

    [POOJA] create a user registration file

---
+++ src/user/registration/Main.java
@@ -0,0 +1,10 @@
+package user.registration;
+
+//import java.util.regex.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration file.");
+
+	}
+}
