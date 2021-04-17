0dbfebaec761e1ee056e1e6721bcf07ae8097ad2
POOJA ROY <59254671+pooja14206@users.noreply.github.com>
2021-04-17 09:33:55 +0530

    Update Main.java

--- src/user/registration/Main.java
+++ src/user/registration/Main.java
@@ -3,13 +3,19 @@ package user.registration;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration file.");
+		/**
+		* creating objects for PatternMatching class
+		*/
 
 		PatternMatching firstName = new PatternMatching();
 		PatternMatching lastName = new PatternMatching();
 		PatternMatching emailId = new PatternMatching();
 		PatternMatching phoneNumber = new PatternMatching();
 		PatternMatching password = new PatternMatching();
-
+		
+		/**
+		* calling the function of PatternMatching class using the onjects.
+		*/
 		firstName.checkPatternFirstName();
 		lastName.checkPatternLastName();
 		emailId.checkPatternEmailId();
